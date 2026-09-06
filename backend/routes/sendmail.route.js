225640ce5f08bb9c8c7dc17215b06046ea943fc3
Radhe Chaudhary <radhechaudhary6398@gmail.com>
2026-09-06 15:19:43 +0530

    resolved SMTP connection error

--- backend/routes/sendmail.route.js
+++ backend/routes/sendmail.route.js
@@ -7,7 +7,10 @@ import db from "../db.js";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    family: 4,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
